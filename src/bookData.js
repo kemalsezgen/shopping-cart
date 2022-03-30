d31688fefae67eb68c1ede2fa7e6dfdb3125af4d
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-03-30 04:28:13 +0300

    book data created

---
+++ src/bookData.js
@@ -0,0 +1,44 @@
+export const bookData = [
+  {
+    id: 1,
+    name: "Momo",
+    author: "Michael Ende",
+    price: 41.99,
+    image: "https://cdn.bkmkitap.com/momo-9441074-26-O.jpg"
+  },
+  {
+    id: 2,
+    name: "Nutuk",
+    author: "Mustafa Kemal Atatürk",
+    price: 22.75,
+    image: "https://cdn.bkmkitap.com/gencler-icin-fotograflarla-nutuk-9497658-14-O.jpg"
+  },
+  {
+    id: 3,
+    name: "Beyaz Gemi",
+    author: "Cengiz Atmatov",
+    price: 13.60,
+    image: "https://cdn.bkmkitap.com/beyaz-gemi-143236-9497927-14-B.jpg"
+  },
+  {
+    id: 4,
+    name: "Sol Ayağım",
+    author: "Christy Brown",
+    price: 27.30,
+    image: "https://cdn.bkmkitap.com/sol-ayagim-287005-8763978-28-B.jpg"
+  },
+  {
+    id: 5,
+    name: "Yaban",
+    author: "Yakup Kadri Karaosmanoğlu",
+    price: 28.56,
+    image: "https://cdn.bkmkitap.com/yaban-10219542-35-B.jpg"
+  },
+  {
+    id: 6,
+    name: "İnci",
+    author: "John Steinbeck",
+    price: 14.86,
+    image: "https://cdn.bkmkitap.com/inci-9497976-19-B.jpg"
+  },
+]
