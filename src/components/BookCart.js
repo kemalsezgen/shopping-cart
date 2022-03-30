cd11e5e8af227a720c48d25ea41882aef42cb291
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-03-30 04:58:52 +0300

    cart and products frame has been created

--- src/components/BookCart.js
+++ src/components/BookCart.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const BookCart = () => {
   return (
-    <div>bookCart</div>
-  )
-}
+    <div>
+      <h2>
+        <Link to="/">Book List</Link> <span>My Cart</span>
+      </h2>
+
+      <h3>Total: 25.99</h3>
+      <div className="book">
+        <img
+          src="https://cdn.bkmkitap.com/gencler-icin-fotograflarla-nutuk-9497658-14-O.jpg"
+          alt="Nutuk"
+        />
+        <div>
+          <h4>Nutuk</h4>
+          <p>Author: Atatürk</p>
+          <p>Price: 25.99</p>
+          <p>Total: 19.99</p>
+          <p>You have 3 of this book in your cart.</p>
+          <button>-</button>
+          <button>Take out</button>
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BookCart;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <h2>
+        <span>Book List</span>
+        <Link to="/bookCart">My Cart</Link>
+      </h2>
+      <div className="book">
+        <img
+          src="https://cdn.bkmkitap.com/gencler-icin-fotograflarla-nutuk-9497658-14-O.jpg"
+          alt="Nutuk"
+        />
+      </div>
+      <div>
+          <h4>Nutuk</h4>
+          <p>Author: Mustafa Kemal Atatürk</p>
+          <p>Fiyat: 25.99</p>
+          <button>Add to the cart</button>
+        </div>
+    </div>
+  );
+};
 
 export default Products;
